32330715a2e8173166f1a28474d165ad5a64e600
brhmv <114811800+brhmv@users.noreply.github.com>
2023-11-08 22:28:47 +0400

    Add files via upload

---
+++ react-1/src/Comp2.js
@@ -0,0 +1,10 @@
+
+function Comp2({name}) {
+  return (
+    <div className="App">        
+        {name}       
+    </div>
+  );
+}
+
+export default Comp2;
---
+++ react-1/src/Comp3.js
@@ -0,0 +1,9 @@
+function Comp3({name}) {
+  return (
+    <div className="App">
+        {name}       
+    </div>
+  );
+}
+
+export default Comp3;
